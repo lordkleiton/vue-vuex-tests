39e15984f5ddbfbc2cd9274826deecd44a44ec15
lordkleiton <m_s_r@outlook.com>
2019-07-28 03:06:44 -0300

    criando componente para exibir filmes

---
+++ src/components/Films.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <div v-if="isReady">
+            <p v-for="film in allFilms" :key="film.id">{{ film.title }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+    name: 'Films',
+    data(){
+        return {
+            isReady: false
+        }
+    },
+    computed: {
+        ...mapGetters(['allFilms'])
+    },
+    created(){
+        this.isReady = true
+        this.fetchFilms()
+    },
+    methods: {
+        ...mapActions(['fetchFilms']),
+    },
+}
+</script>
+
+<style>
+
+</style>
