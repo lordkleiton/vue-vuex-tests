eab9228f4d948a8025f0e9bd1d5886d211ef0e07
lord <m_s_r@outlook.com>
2019-08-02 12:10:31 -0300

    ajustando fetch

---
+++ src/components/Pokemon.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <div v-if="!isReady">
+            Favor aguardar
+        </div>
+        <div v-else>
+            {{currentPokemon.name}}
+        </div>
+    </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex'
+
+export default {
+    data(){
+        return {
+            isReady: false,
+        }
+    },
+    computed: {
+        ...mapGetters(['currentPokemon'])
+    },
+    created(){
+        this.fetchPokemon(this.$route.params.url).then(() => {
+            this.isReady = true
+            console.log(this.currentPokemon)
+        })
+    },
+    methods: {
+        ...mapActions(['fetchPokemon']),
+    },
+}
+</script>
+
+<style>
+
+</style>
